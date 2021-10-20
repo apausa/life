bb67c8196b35ac8f0b5ea3f9eb309a532cdb14b0
pabloapausa <pabloapausa@outlook.com>
2021-10-20 15:50:51 +0200

    Update head tag

--- project/src/life.js
+++ project/src/life.js
@@ -1,18 +1,14 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-plusplus */
 let [first, second] = [[], []];
-let [interval, state] = [null, false];
+let [interval, status] = [null, false];
 const [row, column] = [50, 50];
-const cells = document.querySelectorAll('td');
-const play = document.querySelector('.play');
 
 class Life {
   constructor() {
     const table = document.querySelector('table');
-    for (let x = 0; x <= row; x++) {
+    for (let x = 0; x <= row; x += 1) {
       const tr = document.createElement('tr');
       [first[x], second[x]] = [[], []];
-      for (let y = 0; y <= column; y++) {
+      for (let y = 0; y <= column; y += 1) {
         const td = document.createElement('td');
         td.setAttribute('id', `${x}-${y}`);
         td.setAttribute('class', 'dead');
@@ -31,32 +27,32 @@ class Life {
   }
 
   computer() {
-    for (let x = 1; x < row; x++) {
-      for (let y = 1; y < column; y++) {
+    for (let x = 1; x < row; x += 1) {
+      for (let y = 1; y < column; y += 1) {
         const neighbours = this.find(first, x, y);
-        (neighbours === 3 || (neighbours === 2 && first[x][y] === 1))
-          ? this.isAlive(second, x, y)
-          : this.isDead(second, x, y);
+        if (neighbours === 3 || (neighbours === 2 && first[x][y] === 1)) {
+          this.isAlive(second, x, y);
+        } else this.isDead(second, x, y);
       }
     }
     [first, second] = [second, first];
   }
 
-  isAlive(array, x, y) {
+  isAlive(array, x, y) { // Closure function.
     [this.array, this.x, this.y] = [array, x, y];
     const cell = document.getElementById(`${this.x}-${this.y}`);
     cell.setAttribute('class', 'alive');
     this.array[this.x][this.y] = 1;
   }
 
-  isDead(array, x, y) {
+  isDead(array, x, y) { // Closure function.
     [this.array, this.x, this.y] = [array, x, y];
     const cell = document.getElementById(`${this.x}-${this.y}`);
     cell.setAttribute('class', 'dead');
     this.array[this.x][this.y] = 0;
   }
 
-  find(array, x, y) {
+  find(array, x, y) { // Closure function.
     [this.array, this.x, this.y] = [array, x, y];
     let neighbours = 0;
     neighbours += (first[x - 1][y - 1] === 1) ? 1 : 0; // Top left.
@@ -72,6 +68,8 @@ class Life {
 }
 
 const life = new Life();
+const cells = document.querySelectorAll('td');
+const play = document.querySelector('.play');
 
 cells.forEach((cell) => {
   cell.addEventListener('click', () => {
@@ -80,13 +78,13 @@ cells.forEach((cell) => {
 });
 
 play.addEventListener('click', () => {
-  if (state === false) {
-    state = true;
+  if (status === false) {
+    status = true;
     interval = setInterval(() => {
       life.computer();
     }, 100);
   } else {
-    state = false;
+    status = false;
     clearInterval(interval);
   }
 });
